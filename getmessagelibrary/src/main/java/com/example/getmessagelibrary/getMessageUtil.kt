dd658813058c53523e6ef86125985e6c0906fec4
kbetrandev <kbetrandev@gmail.com>
2018-12-14 11:04:01 -0500

    initial commit

---
+++ getmessagelibrary/src/main/java/com/example/getmessagelibrary/getMessageUtil.kt
@@ -0,0 +1,10 @@
+package com.example.getmessagelibrary
+
+class getMessageUtil {
+    companion object {
+        fun fromMetersToKilometers(): String {
+            return "Library are running"
+        }
+    }
+}
+
